aa5e61165aae9709e5f60e724a466ac9330ca415
Juan Bautista Espino <bautiespino@icloud.com>
2025-05-20 19:24:55 -0300

    init

--- app/api/routes.tsx
+++
@@ -1,5 +0,0 @@
-// Este archivo sirve como punto de entrada para las routes de la API
-// Esta es la estructura sugerida para el backend
-export { default as profileRouter } from "./profiles/route"
-export { default as matchRouter } from "./matches/route"
-export { default as chatRouter } from "./chats/route"
